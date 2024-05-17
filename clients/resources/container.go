69b9371adfde81fefbdb25319d2d14bdd5d76199
ignacio-penas <ignacio.penas4b@gmail.com>
2024-05-17 08:52:51 +0200

    Created a custom package to group all the resource types

---
+++ clients/resources/container.go
@@ -0,0 +1,41 @@
+package resources
+
+type ContainerResource struct {
+}
+
+type ContainerHostResources struct {
+	Memory int     `json:"memory"`
+	Cpus   float32 `json:"cpus"`
+}
+
+type ContainerPorts struct {
+	TargetPort int `json:"target-port"`
+	// Optional fields
+	Protocol      string `json:"protocol"`
+	PublishedPort int    `json:"published-port"`
+}
+
+type ContainerMounts struct {
+	MountType string `json:"mount-type"`
+	Target    string `json:"target"`
+	// Optional fields
+	Source        string              `json:"source"`
+	ReadOnly      bool                `json:"read-only"`
+	VolumeOptions []map[string]string `json:"volume-options"`
+}
+
+type RestartPolicy struct {
+	Condition string `json:"condition"`
+	// Optional fields
+	Delay       int `json:"delay"`
+	MaxAttempts int `json:"max-attempts"`
+	Window      int `json:"window"`
+}
+
+type EnvironmentVariable struct {
+	Name string `json:"name"`
+	// Optional fields
+	Description string `json:"description"`
+	Required    bool   `json:"required"`
+	Value       string `json:"value"`
+}
---
+++ clients/resources/resource_collection.go
@@ -0,0 +1,37 @@
+package resources
+
+import (
+	"encoding/json"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+)
+
+type NuvlaResourceCollection struct {
+	Resources    []map[string]interface{} `json:"resources"`
+	Count        int                      `json:"count"`
+	ResourceName string                   `json:"id"`
+}
+
+// NewCollectionFromResponse creates a NuvlaResourceCollection from a http.Response. It expects the body
+// of the response to be a CimiCollection where key "resources" contains the list of resources.
+func NewCollectionFromResponse(response *http.Response) (*NuvlaResourceCollection, error) {
+	// Create NuvlaResourceCollection from response
+
+	// Unmarshal response body into NuvlaResourceCollection
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Errorf("Error reading response body: %s", err)
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	collection := &NuvlaResourceCollection{}
+	err = json.Unmarshal(body, collection)
+	if err != nil {
+		log.Errorf("Error unmarshaling response body: %s", err)
+		return nil, err
+	}
+
+	return collection, nil
+}
