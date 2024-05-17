4ba7dc4279f16a79bbcedf2f00607ad753058be2
Ignacio Penas Fernandez <11061525+ignacio-penas@users.noreply.github.com>
2024-05-17 09:07:32 +0200

    refactor: Remove resources from clients and point to resources package (#25)

--- clients/user.go
+++ clients/user.go
@@ -3,24 +3,32 @@ package clients
 import (
 	"encoding/json"
 	"github.com/nuvla/api-client-go"
+	"github.com/nuvla/api-client-go/clients/resources"
 	"github.com/nuvla/api-client-go/types"
 	log "github.com/sirupsen/logrus"
 	"io"
 )
 
 type UserClient struct {
-	Client    *api_client_go.NuvlaClient
+	api_client_go.NuvlaClient
 	UserID    *types.NuvlaID
 	SessionID *types.NuvlaID
 }
 
 func NewUserClient(endpoint string, insecure bool, debug bool) *UserClient {
-	return nil
+	sessionOpts := api_client_go.DefaultSessionOpts()
+	sessionOpts.Insecure = insecure
+	sessionOpts.Debug = debug
+	sessionOpts.Endpoint = endpoint
+
+	return &UserClient{
+		NuvlaClient: *api_client_go.NewNuvlaClient(nil, sessionOpts),
+	}
 }
 
 // Add creates a new resource of the given type and returns its ID
 func (c *UserClient) Add(resourceType string, data map[string]interface{}) (*types.NuvlaID, error) {
-	res, err := c.Client.Post(resourceType, data)
+	res, err := c.Post(resourceType, data)
 	if err != nil {
 		log.Errorf("Error adding %s: %s", resourceType, err)
 		return nil, err
@@ -42,21 +50,12 @@ func (c *UserClient) Add(resourceType string, data map[string]interface{}) (*typ
 	return types.NewNuvlaIDFromId(resData["resource-id"].(string)), nil
 }
 
-type SearchOptions struct {
-	First       int      `json:"first"`
-	Filter      string   `json:"filter"`
-	Fields      string   `json:"fields"`
-	Select      []string `json:"select"`
-	OrderBy     string   `json:"orderby"`
-	Aggregation string   `json:"aggregation"`
-}
-
 func (c *UserClient) AddNuvlaEdge(data map[string]interface{}) (*types.NuvlaID, error) {
 	return c.Add("nuvlabox", data)
 }
 
 func (c *UserClient) GetNuvlaEdge(id string, fields []string) (*types.NuvlaResource, error) {
-	return c.Client.Get(id, fields)
+	return c.Get(id, fields)
 }
 
 func (c *UserClient) AddCredential(data map[string]interface{}) (*types.NuvlaID, error) {
@@ -67,8 +66,8 @@ func (c *UserClient) GetId() string {
 	return c.UserID.Id
 }
 
-func (c *UserClient) GetType() ClientResourceType {
-	return UserType
+func (c *UserClient) GetType() resources.NuvlaResourceType {
+	return resources.UserType
 }
 
 func (c *UserClient) GetResourceMap() (map[string]interface{}, error) {
